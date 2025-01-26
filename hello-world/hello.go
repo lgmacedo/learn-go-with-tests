2caf961223157e6dbe0e1ec9b450a9afe9bb6a03
lgmacedo <lucasguerra.macedo@gmail.com>
2025-01-26 14:48:58 -0300

    feat: folder organization, extra languages on Hello World

---
+++ hello-world/hello.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	portuguese = "Portuguese"
+	french     = "French"
+
+	englishHelloPrefix    = "Hello, "
+	portugueseHelloPrefix = "Olá, "
+	frenchHelloPrefix     = "Bonjour, "
+
+	exclamationMark = "!"
+)
+
+func nameIsEmpty(name string) (answer bool) {
+	if name == "" {
+		answer = true
+	} else {
+		answer = false
+	}
+	return
+}
+
+func getGreetingPrefix(language string) (prefix string) {
+	switch language {
+	case portuguese:
+		prefix = portugueseHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name, language string) string {
+	if nameIsEmpty(name) {
+		name = "World"
+	}
+
+	return getGreetingPrefix(language) + name + exclamationMark
+}
+
+func main() {
+	fmt.Println(Hello("Lucas", "Portuguese"))
+}
---
+++ hello-world/hello_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Lucas", "")
+		want := "Hello, Lucas!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Portuguese", func(t *testing.T) {
+		got := Hello("Elodie", "Portuguese")
+		want := "Olá, Elodie!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jean", "French")
+		want := "Bonjour, Jean!"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
